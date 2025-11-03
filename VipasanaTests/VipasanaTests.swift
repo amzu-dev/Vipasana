43bddab212a70443e7a077ba8c5769cdd836bde9
Venkatesh Balakumar <venkat@amzuit.com>
2025-11-03 16:23:35 +0000

    Initial Commit

---
+++ VipasanaTests/VipasanaTests.swift
@@ -0,0 +1,16 @@
+//
+//  VipasanaTests.swift
+//  VipasanaTests
+//
+//  Created by VENKATESH BALAKUMAR on 03/11/2025.
+//
+
+import Testing
+
+struct VipasanaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
